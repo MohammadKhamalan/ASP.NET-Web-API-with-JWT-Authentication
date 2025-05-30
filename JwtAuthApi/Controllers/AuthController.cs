88a5a94616b07190289973618342c690f03a9e62
MohammadKhamalan <mohammad.khamalan@gmail.com>
2025-05-30 05:41:57 +0300

    Implementing and Configuring JWT Authentication

---
+++ JwtAuthApi/Controllers/AuthController.cs
@@ -0,0 +1,49 @@
+﻿using JwtAuthApi.Helpers;
+using JwtAuthApi.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController: ControllerBase
+    {
+        private readonly JwtTokenGenerator _tokenGenerator;
+
+        public AuthController(JwtTokenGenerator tokenGenerator)
+        {
+            _tokenGenerator = tokenGenerator;
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if(request.Username=="mohammad" && request.Password == "12345")
+            {
+                var token = _tokenGenerator.GenerateToken(request.Username);
+                return Ok(new { Token = token });
+            }
+            return Unauthorized("Invalid credentials");
+        }
+
+        [HttpPost("validate")]
+        public IActionResult Validate([FromBody] string token)
+        {
+            var principal = _tokenGenerator.ValidateToken(token);
+            if (principal == null)
+                return Unauthorized("Token is invalid or expired.");
+
+            return Ok($"Token is valid. Hello, {principal.Identity?.Name}!");
+        }
+
+        [Authorize]
+        [HttpGet("welcome")]
+        public IActionResult welcome()
+        {
+            return Ok($"Welcome, {User.Identity?.Name}! Today’s weather is sunny.");
+
+        }
+    }
+}
---
+++ JwtAuthApi/Helpers/JwtTokenGenerator.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace JwtAuthApi.Helpers
+{
+    public class JwtTokenGenerator
+    {
+        private readonly string _secretKey;
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly int _expiryInHours;
+
+        public JwtTokenGenerator(IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("JwtSettings");
+            _secretKey = jwtSettings.GetValue<string>("SecretKey");
+            _issuer = jwtSettings.GetValue<string>("Issuer");
+            _audience = jwtSettings.GetValue<string>("Audience");
+            _expiryInHours = jwtSettings.GetValue<int>("ExpiryInHours");
+        }
+
+        public string GenerateToken(string userName)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, userName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _issuer,
+                audience: _audience,
+                claims: claims,
+                expires: DateTime.Now.AddHours(_expiryInHours),
+                signingCredentials: creds);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public ClaimsPrincipal? ValidateToken(string token)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(_secretKey);
+
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = _issuer,
+                    ValidateAudience = true,
+                    ValidAudience = _audience,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return principal;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
